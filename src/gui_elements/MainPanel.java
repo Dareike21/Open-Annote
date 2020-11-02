3c7b2d04d5a775ebb3ca6969d3e1263c81105a5d
hossboyz <42874145+hossboyz@users.noreply.github.com>
2020-11-02 16:41:07 -0500

    The textfield is now operational and HTML enabled.

--- src/gui_elements/MainPanel.java
+++ src/gui_elements/MainPanel.java
@@ -1,14 +1,17 @@
 package gui_elements;
 
 import javax.swing.*;
+
+import main.Tester;
+
 import java.awt.*;
 
 public class MainPanel extends JPanel {
 	
 	private JTabbedPane tabs;
 	
-	private JPanel read_tab;
-	private JPanel edit_tab;
+	private SplitPanel read_tab;
+	private SplitPanel edit_tab;
 	
 	public MainPanel() {
 		setLayout(new GridLayout(1,1));
@@ -22,7 +25,14 @@ public class MainPanel extends JPanel {
 		
 		edit_tab = new SplitPanel();
 		ImageIcon edit_icon = new ImageIcon("assets/images/edit.png");
-		tabs.addTab("<html><font size=+1>edit</font></html>", edit_icon, edit_tab);
+		tabs.addTab("<html><font size=+1>Edit</font></html>", edit_icon, edit_tab);
+		
+		test();
+	}
+	
+	private void test() {
+		read_tab.set_doc_field(Tester.lorem_ipsum);
+		read_tab.set_doc_field_font(20);
 	}
 	
 }
--- src/gui_elements/SplitPanel.java
+++ src/gui_elements/SplitPanel.java
@@ -1,19 +1,26 @@
 package gui_elements;
 
 import javax.swing.*;
+import javax.swing.text.*;
+import javax.swing.text.html.*;
+
 import java.awt.*;
+import java.io.IOException;
 
 public class SplitPanel extends JPanel {
 
 	private static final long serialVersionUID = 2827997432620163107L;
 	
-	public JPanel panel;
-	public JMenuBar doc_menu;
-	public JMenuBar ano_menu;
-	public JPanel doc_panel;
-	public JPanel ano_panel;
-	public JMenu open_doc;
-	public JMenu open_ano;
+	private JPanel panel;
+	private JMenuBar doc_menu;
+	private JMenuBar ano_menu;
+	private JPanel doc_panel;
+	private JPanel ano_panel;
+	private JMenu open_doc;
+	private JMenu edit_open_ano;
+	private JTextPane doc_field;
+	
+	private StyledDocument doc;
 	
 	public SplitPanel() {
 		setBackground(Color.DARK_GRAY);
@@ -49,8 +56,8 @@ public class SplitPanel extends JPanel {
 		gbc_edit_ano_menu.gridy = 0;
 		panel.add(ano_menu, gbc_edit_ano_menu);
 		
-		open_ano = new JMenu("Open annotation");
-		ano_menu.add(open_ano);
+		edit_open_ano = new JMenu("Open annotation");
+		ano_menu.add(edit_open_ano);
 		
 		doc_panel = new JPanel();
 		doc_panel.setBorder(null);
@@ -63,6 +70,19 @@ public class SplitPanel extends JPanel {
 		gbc_edit_doc_panel.gridx = 0;
 		gbc_edit_doc_panel.gridy = 1;
 		panel.add(doc_panel, gbc_edit_doc_panel);
+		SpringLayout sl_doc_panel = new SpringLayout();
+		doc_panel.setLayout(sl_doc_panel);
+		
+		JScrollPane scroll_pane = new JScrollPane();
+		sl_doc_panel.putConstraint(SpringLayout.NORTH, scroll_pane, 0, SpringLayout.NORTH, doc_panel);
+		sl_doc_panel.putConstraint(SpringLayout.WEST, scroll_pane, 50, SpringLayout.WEST, doc_panel);
+		sl_doc_panel.putConstraint(SpringLayout.SOUTH, scroll_pane, 0, SpringLayout.SOUTH, doc_panel);
+		sl_doc_panel.putConstraint(SpringLayout.EAST, scroll_pane, -50, SpringLayout.EAST, doc_panel);
+		doc_panel.add(scroll_pane);
+		
+		doc_field = new JTextPane(new HTMLDocument());
+		doc_field.setEditable(false);
+		scroll_pane.setViewportView(doc_field);
 		
 		ano_panel = new JPanel();
 		ano_panel.setBorder(null);
@@ -75,5 +95,32 @@ public class SplitPanel extends JPanel {
 		gbc_edit_ano_panel.gridx = 1;
 		gbc_edit_ano_panel.gridy = 1;
 		panel.add(ano_panel, gbc_edit_ano_panel);
+		
+		///
+		try {
+			init_doc_field();
+		} catch (BadLocationException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	private void init_doc_field() throws BadLocationException, IOException {
+		HTMLEditorKit kit = new HTMLEditorKit();
+	    HTMLDocument doc = new HTMLDocument();
+	    doc_field.setEditorKit(kit);
+	    doc_field.setDocument(doc);
+	    //kit.insertHTML(doc, doc.getLength(), "<b>hello", 0, 0, HTML.Tag.B);
+	    //kit.insertHTML(doc, doc.getLength(), "<font color='red'><u>world</u></font>", 0, 0, null);
+		
+	}
+
+	public void set_doc_field(String contents) {
+		System.out.println(contents);
+		doc_field.setText(contents);
+	}
+	
+	public void set_doc_field_font(int size) {
+		
 	}
 }
